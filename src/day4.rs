a7d03867db70da447aa6a6bcd358cdb4029cfab8
Andrew Marino <andrewymarino@gmail.com>
2023-12-04 14:19:51 -0800

    Improve day4 impl

--- src/day4.rs
+++ src/day4.rs
@@ -1,15 +1,19 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 
-fn n_winners(card: &str) -> u32 {
+fn n_winners(card: &str) -> usize {
     let card = card.split_once(": ").unwrap().1;
-    let mut card_lists = card.split(" | ").map(|list| {
-        list.split_whitespace()
-            .map(|s| s.parse::<u32>().unwrap())
-            .collect::<HashSet<_>>()
-    });
-    let winners = card_lists.next().unwrap();
-    let yours = card_lists.next().unwrap();
-    winners.intersection(&yours).count() as u32
+    let (winners, yours) = card
+        .split(" | ")
+        .map(|list| {
+            list.split_whitespace()
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect::<HashSet<_>>()
+        })
+        .tuples()
+        .next()
+        .unwrap();
+    winners.intersection(&yours).count()
 }
 
 pub fn soln_4_1() -> u32 {
@@ -17,13 +21,8 @@ pub fn soln_4_1() -> u32 {
         .unwrap()
         .lines()
         .map(n_winners)
-        .map(|n_winners| {
-            if n_winners == 0 {
-                0
-            } else {
-                2u32.pow(n_winners - 1)
-            }
-        })
+        .filter(|n_winners| *n_winners != 0)
+        .map(|n_winners| 2u32.pow(n_winners as u32 - 1))
         .sum()
 }
 
@@ -36,9 +35,9 @@ pub fn soln_4_2() -> u32 {
     let mut n_cards = vec![1u32; n_winners.len()];
     for (i, n) in n_winners.iter().enumerate() {
         let n_copies = n_cards[i];
-        for j in 1..=*n {
-            n_cards[i + j as usize] += n_copies;
-        }
+        n_cards[(i + 1)..=(i + *n)]
+            .iter_mut()
+            .for_each(|n| *n += n_copies);
     }
 
     n_cards.iter().sum()
